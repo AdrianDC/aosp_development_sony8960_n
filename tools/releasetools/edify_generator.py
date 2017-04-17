464af36dd8e25fddc43e72b63d8ffe224517dff5
Adrian DC <radian.dc@gmail.com>
2017-04-17 10:15:37 +0200

    build: ota: Support for install tools in /tmp/install
     * Adapted to AOSP from the following commits:
    
    build: ota: Support for install tools in /tmp/install
    
     * Anything in OUT/install gets packaged up into the zip and extracted
       to /tmp/install immediately after FullOTA_InstallBegin.
    
     * Use /tmp/install in edify scripts and remove code related to using
       and manipulating /system for install tools.
    
    Change-Id: I315a3238e36c8d15e26f935e272f7e27dd59c320
    
    build: Update install tools packaging for target-files support
    
    Modifies "build: ota: Support for install tools in /tmp/install" to
    support signing steps being split from build steps.
    
    Package install files into target-files INSTALL path
    Read from target-files for OTA package creation
    
    Change-Id: I64f919c2a757b5474f6cc5f82bd6c33c2a8b558a
    
    Change-Id: I57b27a9d0503483a973ba54cc9b2466b51fa7a39

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -139,7 +139,7 @@ class EdifyGenerator(object):
     self.script.append(cmd)
 
   def AssertSomeBootloader(self, *bootloaders):
-    """Asert that the bootloader version is one of *bootloaders."""
+    """Assert that the bootloader version is one of *bootloaders."""
     cmd = ("assert(" +
            " ||\0".join(['getprop("ro.bootloader") == "%s"' % (b,)
                          for b in bootloaders]) +
@@ -147,9 +147,7 @@ class EdifyGenerator(object):
     self.script.append(self.WordWrap(cmd))
 
   def RunBackup(self, command):
-    self.script.append('package_extract_file("system/bin/backuptool.sh", "/tmp/backuptool.sh");')
-    self.script.append('set_perm(0, 0, 0777, "/tmp/backuptool.sh");')
-    self.script.append(('run_program("/tmp/backuptool.sh", "%s");' % command))
+    self.script.append(('run_program("/tmp/install/bin/backuptool.sh", "%s");' % command))
 
   def ShowProgress(self, frac, dur):
     """Update the progress bar, advancing it over 'frac' over the next
@@ -221,6 +219,12 @@ class EdifyGenerator(object):
           p.fs_type, common.PARTITION_TYPES[p.fs_type], p.device,
           p.mount_point, mount_flags))
       self.mounts.add(p.mount_point)
+
+  def Unmount(self, mount_point):
+    """Unmount the partiiton with the given mount_point."""
+    if mount_point in self.mounts:
+      self.mounts.remove(mount_point)
+      self.script.append('unmount("%s");' % (mount_point,))
 
   def UnpackPackageDir(self, src, dst):
     """Unpack a given directory from the OTA package into the given
